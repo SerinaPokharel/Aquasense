574b7abbfd3975fa7f50ef9a9b2c8b44d62549a2
Sairash <sairash@buuzz.co.uk>
2023-11-27 18:53:36 +0545

    mongoose comment removed

--- server/index.js
+++ server/index.js
@@ -77,19 +77,19 @@ app.get("/", (req, res) => {
 });
 
 /* MONGOOSE SETUP */
-// const PORT = process.env.PORT || 3001;
-// mongoose
-//   .connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+const PORT = process.env.PORT || 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-//     /* ADD DATA ONE TIME */
-//     // User.insertMany(users);
-//     // Post.insertMany(posts);
-//   })
-//   .catch((error) => console.log(`${error} did not connect`));
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
 
-app.listen(5000, () => console.log(`Server Port: ${5000}`));
+// app.listen(5000, () => console.log(`Server Port: ${5000}`));
